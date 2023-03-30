23f56030d6c193145b5cbd60f76d2243b3fe74b9
dpears04 <dpears04@uoguelph.ca>
2023-03-30 19:59:48 -0400

    Added locking room doors + new room prefabs to speed up creation

--- Assets/Scripts/Rooms/EnemySpawn.cs
+++ Assets/Scripts/Rooms/EnemySpawn.cs
@@ -7,13 +7,23 @@ public class EnemySpawn : MonoBehaviour
     public Transform spawnPoint;
     public GameObject prefab;
     public int numToSpawn = 2;
+    GameObject[] doors;
+    GameObject[] enemies;
 
+    private void Awake()
+    {
+        doors = GameObject.FindGameObjectsWithTag("Door");
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            foreach (GameObject door in doors)
+            {
+                door.SetActive(true);
+            }
             // If the room is not cleared entirely it will respawn the room 
-            if(numToSpawn != 0)
+            if (numToSpawn != 0)
             {
                 numToSpawn = Random.Range(numToSpawn, numToSpawn + 5);
             }
@@ -43,5 +53,16 @@ public class EnemySpawn : MonoBehaviour
 
         }
 
+    }
+    private void Update()
+    {
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        if (enemies.Length == 0)
+        {
+            foreach (GameObject door in doors)
+            {
+                door.SetActive(false);
+            }
+        }
     }
 }
